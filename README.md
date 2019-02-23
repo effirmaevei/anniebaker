222259c8de8fc5dd1e24d52180863c93eba2540a
effirmaevei <effirmaevei@gmail.com>
2019-02-23 09:33:43 -0500

    Restructure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cloud
+Source for <a href=effirmaevei.github.io/cloud>effirmaevei.github.io/cloud</a>
--- main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-    var x = 56; // $('body').css('padding-top');
-    $('.header').height($(window).height() - x);
-    $('body').css('padding-top', x);
-    console.log(2323);
-})
